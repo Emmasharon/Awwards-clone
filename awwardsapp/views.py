da258c7acb1f80d091d2467935eec55347833fba
Emma Sharon <emmasharon19@gmail.com>
2019-08-02 10:47:25 +0300

    create base.html, register url for index.html in urls.py file

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+# from .models import Profile,Post,Following,Comment,User
+from .forms import ProfileForm,PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+# from django.db.models import F
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
