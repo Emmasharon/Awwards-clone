d93c764003e0a9339ff727db073670e5ebe048c7
Emma Sharon <emmasharon19@gmail.com>
2019-08-02 18:38:36 +0300

    include navbar in base.html,update index.html to include navbar, create navbar.html,update base.html file

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-# from .models import Profile,Post,Following,Comment,User
+from .models import Profile,Project
 # from .forms import ProfileForm,PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
-# from django.db.models import F
+from django.db.models import F
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required(login_url='/accounts/login/')
 def profile(request):
-    return render(request, 'profile.html')
+    def profile(request):
+        current_user = request.user
+        if request.method == 'POST':
+            profileform = ProfileForm(request.POST, request.FILES)
+            if profileform.is_valid():
+                profile = profileform.save(commit=False)
+                profile.user = current_user
+                profile.save()
+                
+            return redirect('profile')
+
+        else:
+            profileform = ProfileForm()
+            
+        return render(request, 'profile.html',locals())
+       
